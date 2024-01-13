d0e7abaf812f7824dbb79d9fce3f0862ff1d4fa9
Mundal1994 <manga_flip@hotmail.com>
2024-01-13 22:11:15 +0200

    deleted comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,7 @@
 
 std::vector<bool> ThreadSimulation::m_forks {{false, false}};
 std::mutex ThreadSimulation::m_mutex;
-/*
 
-philosopherBase {
-    time_to_die_timer
-    time_to_eat_timer
-    time_to_sleep_timer
-}
-
-cyclus: eat --> sleep --> think --> eat
-simulation stops if philospher dies of starvation or they have eaten enough times
-
-
-Your(s) program(s) should take the following arguments:
-number_of_philosophers time_to_die time_to_eat time_to_sleep
-[number_of_times_each_philosopher_must_eat]
-◦ number_of_philosophers: The number of philosophers and also the number
-of forks.
-◦ time_to_die (in milliseconds): If a philosopher didn’t start eating time_to_die
-milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
-◦ time_to_eat (in milliseconds): The time it takes for a philosopher to eat.
-During that time, they will need to hold two forks.
-◦ time_to_sleep (in milliseconds): The time a philosopher will spend sleeping.
-◦ number_of_times_each_philosopher_must_eat (optional argument): If all
-philosophers have eaten at least number_of_times_each_philosopher_must_eat
-times, the simulation stops. If not specified, the simulation stops when a
-philosopher dies.
-• Each philosopher has a number ranging from 1 to number_of_philosophers.
-• Philosopher number 1 sits next to philosopher number number_of_philosophers.
-Any other philosopher number N sits between philosopher number N - 1 and philosopher number N + 1.
-
-*/
-
-// Your(s) program(s) should take the following arguments:
-// number_of_philosophers time_to_die time_to_eat time_to_sleep
-// [number_of_times_each_philosopher_must_eat]
-
-
-#include <chrono>
-#include <iostream>
-#include <thread>
 void printUsage(void) {
     std::cout << "Usage: ./philo number_of_philosphers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]" << std::endl;
     std::cout << "[number_of_times_each_philosopher_must_eat] is optional" << std::endl;
